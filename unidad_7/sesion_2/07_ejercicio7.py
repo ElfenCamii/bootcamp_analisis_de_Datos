bac97f786a02aa922bc338989e8e3690126317f6
Camiilo Piinto <camiilo.piinto@gmail.com>
2025-09-10 16:25:50 -0500

    feat(exercises): add solution for exercise 7 compatible with the platform

--- unidad_7/sesion_2/07_ejercicio7.py
+++ unidad_7/sesion_2/07_ejercicio7.py
@@ -7,3 +7,16 @@
 # cuya longitud este comprendida entre 1.20 y 1.30 son aptas. 
 # Imprimir la cantidad de piezas del lote que son aptas
 ###
+
+# Intento para la plataforma
+
+import os
+os.system('cls')
+n_piezas = int(input('Ingrese la cantidad de piezas a procesar: '))
+n = []
+
+for piezas in range(0, n_piezas):
+    l_piezas = float(input('Ingrese la longitud de la pieza en metros: '))
+    if 1.2 <= l_piezas <= 1.3:
+        n.append(l_piezas)
+print(f'La cantidad de piezas aptas es: {len(n)}')
