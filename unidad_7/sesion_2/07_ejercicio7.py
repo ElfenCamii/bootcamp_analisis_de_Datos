11b497f871665808934cc6b2454a39617e5787e1
Camiilo Piinto <camiilo.piinto@gmail.com>
2025-09-10 16:12:41 -0500

    docs(exercises): add 07_ejercicio7.py with the statement of exercise 7

---
+++ unidad_7/sesion_2/07_ejercicio7.py
@@ -0,0 +1,9 @@
+###
+# Ejercicio 7
+# Perfiles de hierro:
+# Una planta que fabrica perfiles de hierro posee un lote de n piezas
+# Diseñar un programa que pida ingresar por teclado la cantidad de piezas 
+# a procesar e ingrese la longitud de cada perfil, sabiendo que la pieza 
+# cuya longitud este comprendida entre 1.20 y 1.30 son aptas. 
+# Imprimir la cantidad de piezas del lote que son aptas
+###
