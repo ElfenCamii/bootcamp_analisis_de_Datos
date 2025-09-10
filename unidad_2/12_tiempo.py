a20ab9c8a382538472dd6b0c1222f07de1383f70
Camiilo Piinto <camiilo.piinto@gmail.com>
2025-09-10 14:00:52 -0500

    feat(time): add full implementation of 12_tiempo.py with practical examples
    - Added all the code for the file `12_tiempo.py`
    - Included practical examples to demonstrate usage
    - Added explanation for exercise 1 within the same file

--- unidad_2/12_tiempo.py
+++ unidad_2/12_tiempo.py
@@ -10,21 +10,48 @@
 #   date: represe4nta solo fechas
 #   time: representa solo horas
 
-
-
-
-
-
-
-
-
-
-
-
-
 import os
 import math
+import datetime
 os.system('cls')
+
+# Fecha y hora actual
+ahora = datetime.datetime.now()
+print(f'Fecha y hora actual: {ahora}')        # Salida: 2025-09-10 11:20:45.095540
+
+# Formato personalizado
+print(f'Formato personalizado: {ahora.strftime('%Y-%m-%d')}')
+
+# Diferencia entre fechas
+fecha_1 = datetime.date(2025, 1, 1)
+fecha_2 = datetime.date(2025, 9, 10)
+diferencia = fecha_2 - fecha_1
+print(f'La diferencia entre la fecha 1: {fecha_1} y la fecha 2: {fecha_2} es: {diferencia}')
+
+# Conversión de cadena a fechas
+fecha_str = '2024-09-10'
+fecha = datetime.datetime.strptime(fecha_str, '%Y-%m-%d')
+print(fecha)
+
+###
+# Ejemplo 1:
+# Agrupar datos por mes
+###
+
+fechas = ['2025-01-01', '2025-02-15', '2025-02-28']
+formateadas = [datetime.datetime.strptime(fecha, '%Y-%m-%d') for fecha in fechas]
+meses = [fecha.strftime('%B') for fecha in formateadas]
+print(meses)        # Salida: ['January', 'February', 'February']
+
+###
+# Ejercicio 1:
+# Tienes una lista de ventas mensaules de una empresa, y necesitas:
+#   Calcular el crecimiento porcentual entre cada mes.
+#   Normalizar las ventas utilizando el logaritmo natural.
+#   Escribir funciones que realiza cada tarea
+#  Datos de entrada:
+#   ventas = [100, 120, 150, 200, 300, 450]
+###
 
 def crecimiento_porcentual(lista):
     crecimientos = []
