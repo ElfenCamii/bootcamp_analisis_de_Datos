3a049f2bd3e8d1ca33425dec4ea5e93a39fe4f55
Camiilo Piinto <camiilo.piinto@gmail.com>
2025-09-17 15:25:10 -0500

    feat(exercises): added base solution for session 3 exercise 7

--- unidad_7/sesion_3/07_ejercicio7.py
+++ unidad_7/sesion_3/07_ejercicio7.py
@@ -4,3 +4,12 @@
 # Escribir un programa que pregunte al usuario una cantidad a invertir, 
 # el interés anual y el número de años, y muestre por pantalla el capital 
 # obtenido en la inversión cada año que dura la inversión.
+###
+
+user_invertion = float(input(': '))
+user_fet = float(input('::'))
+years = float(input(':::'))
+
+capital_final = user_invertion * (1 + (user_fet/100))**years
+
+print(capital_final)
